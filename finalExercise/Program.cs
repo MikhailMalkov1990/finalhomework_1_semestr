b36554400d5629bd76717042f91810854bff70ca
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-12-23 18:19:38 -0600

    commited finalExercise

---
+++ finalExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] primaryArray = new string[4] {"hello", "2", "world", ":-)"};
+string[] newArray = new string[primaryArray.Length];
+void FillArray(string[] primaryArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < primaryArray.Length; i++)
+    {
+        if (primaryArray[i].Length <=3)
+        {
+            newArray[count] = primaryArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] newArray)
+{
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write($"{newArray[i]} ");
+    }
+    System.Console.WriteLine();
+}
+FillArray(primaryArray, newArray);
+PrintArray(newArray);
